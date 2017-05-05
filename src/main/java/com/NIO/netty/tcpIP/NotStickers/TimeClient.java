aba85e118e7b2af3cabd87e3696642c8f8cdc104
chenssy89 <chenssy995812509@163.com>
2017-05-05 08:58:39 +0800

    TCP/IP 未处理粘包拆包问题

---
+++ src/main/java/com/NIO/netty/tcpIP/NotStickers/TimeClient.java
@@ -0,0 +1,46 @@
+package main.java.com.NIO.netty.tcpIP.NotStickers;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TimeClient {
+
+    public void connect(int port, String host) {
+        // 配置客户端线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        Bootstrap b = new Bootstrap();
+
+        try {
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+            // 发起异步连接操作
+            ChannelFuture future = b.connect(host, port);
+
+            // 等待客户端链路关闭
+
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TimeClient().connect(8899,"127.0.0.1");
+    }
+}
---
+++ src/main/java/com/NIO/netty/tcpIP/NotStickers/TimerServerHandler.java
@@ -0,0 +1,45 @@
+package main.java.com.NIO.netty.tcpIP.NotStickers;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+
+/**
+ * 未考虑粘包拆包
+ *
+ * @author chenssy
+ * @date 2017-05-05
+ * @since v1.0.0
+ */
+public class TimerServerHandler extends ChannelHandlerAdapter{
+
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+
+        String body = new String(req,"UTF-8").substring(0,req.length - System.getProperty("line.separator").length());
+
+        System.out.println("The time server receive order:" + body + "; the counter is " + (++counter));
+
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+
+        currentTime = currentTime + System.getProperty("line.separator");
+
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}

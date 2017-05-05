ed82fa947b9122d4017653b664d22f595da009d4
chenssy89 <chenssy995812509@163.com>
2017-05-05 12:41:35 +0800

    LineBasedFrameDecoder StringDecoder 解决粘包拆包问题

---
+++ src/main/java/com/NIO/netty/tcpIP/stickers/TimeClientHandler.java
@@ -0,0 +1,49 @@
+package main.java.com.NIO.netty.tcpIP.stickers;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class TimeClientHandler extends ChannelHandlerAdapter{
+
+    private int counter;
+
+    private byte[] req;
+
+
+    public TimeClientHandler(){
+        req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
+    }
+    
+    
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ByteBuf message = null;
+
+        for (int i = 0 ; i < 100 ; i++){
+            message = Unpooled.buffer(req.length);
+            message.writeBytes(req);
+
+            ctx.writeAndFlush(message);
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        
+        String body = (String) msg;
+        
+        System.out.println("Now is :" + body + "; the conters is :" + (++counter));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        
+        System.out.println("cause by :" + cause.getMessage());
+        
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/NIO/netty/tcpIP/stickers/TimerServerHandler.java
@@ -0,0 +1,40 @@
+package main.java.com.NIO.netty.tcpIP.stickers;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+
+/**
+ * 未考虑粘包拆包
+ *
+ * @author chenssy
+ * @date 2017-05-05
+ * @since v1.0.0
+ */
+public class TimerServerHandler extends ChannelHandlerAdapter{
+
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+
+        System.out.println("The time server receive order:" + body + "; the counter is :" + (++counter));
+
+        String currentTime = "  QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+
+        currentTime = currentTime + System.getProperty("line.separator");
+
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
